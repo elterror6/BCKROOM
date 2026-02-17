5fbc1bd1e546bdca54cde796bea12780379adb0f
elterror6 <danibasketball2000@gmail.com>
2026-02-17 19:58:56 +0100

    feat(#2): Realización de la base del proyecto Maven.

---
+++ bckroom/src/main/java/es/uclm/bckroom/BckroomApplication.java
@@ -0,0 +1,13 @@
+package es.uclm.bckroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BckroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BckroomApplication.class, args);
+	}
+
+}
