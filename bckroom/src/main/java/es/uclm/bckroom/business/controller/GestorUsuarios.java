5faf0b82fcf4ecf2d9032be98557e2905dca4b38
elterror6 <danibasketball2000@gmail.com>
2026-02-22 13:18:54 +0100

    feat(#7): Realizado el mapeo GET de la página de loginç.

--- bckroom/src/main/java/es/uclm/bckroom/business/controller/GestorUsuarios.java
+++ bckroom/src/main/java/es/uclm/bckroom/business/controller/GestorUsuarios.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.uclm.bckroom.business.domain.Inquilino;
+import es.uclm.bckroom.business.domain.Propietario;
 import es.uclm.bckroom.business.domain.Usuario;
 import es.uclm.bckroom.persistence.UsuarioDAO;
+import es.uclm.bckroom.persistence.PropietarioDAO;
+import es.uclm.bckroom.persistence.InquilinoDAO;
 
 @Controller
 public class GestorUsuarios implements IGestorUsuarios{
@@ -19,6 +23,10 @@ public class GestorUsuarios implements IGestorUsuarios{
 	
 	@Autowired
 	private UsuarioDAO usuarioDAO;
+	@Autowired
+	private PropietarioDAO propietarioDAO;
+	@Autowired
+	private InquilinoDAO inquilinoDAO;
 	
 	@GetMapping("/register")
 	public String registerForm(Model model) {
@@ -29,11 +37,22 @@ public class GestorUsuarios implements IGestorUsuarios{
 	@PostMapping("/register")
 	public String inquilinoSubmit(@ModelAttribute Usuario usuario, @RequestParam String tipo,Model model) {
 		model.addAttribute("usuario", usuario);
-		Usuario guardarUsuario = usuarioDAO.save(usuario);
-		log.info("Usuario Guardado: "+guardarUsuario);
 		if (tipo.equals(inquilino)) {
-			return "login";
+			Inquilino nuevoInquilino = new Inquilino(usuario);
+			log.info("Inquilino creado: "+nuevoInquilino.getUsername());
+			inquilinoDAO.save(nuevoInquilino);
+		} else {
+			Propietario nuevoPropietario = new Propietario(usuario);
+			log.info("Propietario creado: "+nuevoPropietario.getUsername());
+			propietarioDAO.save(nuevoPropietario);
 		}
-		return "propietario/registrar-inmuebles";
+		return "login";
+	}
+	
+	@GetMapping("/login")
+	public String loginForm(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		log.info(usuarioDAO.findAll().toString());
+		return "login";
 	}
 }
--- bckroom/src/main/java/es/uclm/bckroom/persistence/ListaDeseosDAO.java
+++ bckroom/src/main/java/es/uclm/bckroom/persistence/ListaDeseosDAO.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import es.uclm.bckroom.business.domain.ListaDeseos;
 
 public interface ListaDeseosDAO extends JpaRepository<ListaDeseos, Long> {
-
+	
 }
