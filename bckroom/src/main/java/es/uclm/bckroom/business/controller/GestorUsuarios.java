360f5fb8d354438cda2c6169fba894e223276529
elterror6 <danibasketball2000@gmail.com>
2026-04-15 21:52:57 +0200

    doc(#31): Refactorizado frontend de la funcionalidad de registro.

--- bckroom/src/main/java/es/uclm/bckroom/business/controller/GestorUsuarios.java
+++ bckroom/src/main/java/es/uclm/bckroom/business/controller/GestorUsuarios.java
@@ -1,32 +1,20 @@
 package es.uclm.bckroom.business.controller;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import es.uclm.bckroom.business.domain.Inmueble;
-import es.uclm.bckroom.business.domain.Inquilino;
-import es.uclm.bckroom.business.domain.Propietario;
 import es.uclm.bckroom.business.domain.TipoCalle;
 import es.uclm.bckroom.business.domain.Usuario;
 import es.uclm.bckroom.business.dto.RegistroUsuarioDTO;
+import es.uclm.bckroom.business.dto.TipoUsuario;
 import es.uclm.bckroom.business.services.ServicioUsuario;
-import es.uclm.bckroom.persistence.UsuarioDAO;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-import es.uclm.bckroom.persistence.PropietarioDAO;
-import es.uclm.bckroom.persistence.InquilinoDAO;
 
 @Controller
 public class GestorUsuarios implements IGestorUsuarios{
